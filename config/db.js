fbaccf534fabc37de3f9576cc4da75cfe3970254
Muhamad Nurul <rullzfourteen@gmail.com>
2026-07-31 22:37:53 +0700

    fix db.js

--- config/db.js
+++ config/db.js
@@ -1,15 +1,17 @@
 const mysql = require('mysql2/promise');
-require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'cibuniwangi',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME || 'defaultdb',
+  port: process.env.DB_PORT || 22308,
+  ssl: {
+    rejectUnauthorized: false // Penting agar Aiven tidak menolak koneksi SSL dari Vercel
+  },
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0,
-  dateStrings: true
+  queueLimit: 0
 });
 
 module.exports = pool;
